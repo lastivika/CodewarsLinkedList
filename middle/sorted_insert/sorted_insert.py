db350fb5c47d5cd10375d322ed99d9ad640fc511
Yuliia Oliinyk <yuliya.oliynyk.3107@gmail.com>
2026-03-22 19:41:12 +0200

    created module 'sorted_insert'

--- middle/sorted_insert/sorted_insert.py
+++ middle/sorted_insert/sorted_insert.py
@@ -1,13 +1,19 @@
 '''module sorted insert'''
-class Node(object):
-    '''class node'''
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-
 def sorted_insert(head, data):
-    cur = head
-    if cur == cur.next:
-        cur.next
+    '''
+    inserts a node into the correct location of
+    a pre-sorted linked list which is sorted in ascending order
+    '''
+    n_node = Node(data)
+    if not head or data < head.data:
+        n_node.next = head
+        return n_node
+
+    curr = head
+    while curr.next and curr.next.data < data:
+        curr = curr.next
+
+    n_node.next = curr.next
+    curr.next = n_node
+
     return head
